e92b027cac33903a9e48006cb2c0a7650fcae330
65021543Natthanan <155602265+65021543Natthanan@users.noreply.github.com>
2024-02-12 01:03:04 +0700

    Add files via upload

--- shoper/sign up/script.js
+++ shoper/sign up/script.js
@@ -1,35 +1,34 @@
-// script.js
-
 document.addEventListener('DOMContentLoaded', function () {
-  var addressModal = new bootstrap.Modal(document.getElementById('addressModal'));
+    var addressModal = new bootstrap.Modal(document.getElementById('addressModal'));
 
-  document.getElementById('addressForm').addEventListener('submit', function (event) {
-      event.preventDefault();
-      saveDataAndCloseModal();
-  });
-
-  // Event listener สำหรับปิด Modal จากปุ่มปิด
-  var closeModalButtons = document.querySelectorAll('[data-bs-dismiss="modal"]');
-  closeModalButtons.forEach(function (button) {
-      button.addEventListener('click', function () {
-          addressModal.hide();
-      });
-  });
+    // Event listener สำหรับปิด Modal จากปุ่มปิด
+    var closeModalButtons = document.querySelectorAll('[data-bs-dismiss="modal"]');
+    closeModalButtons.forEach(function (button) {
+        button.addEventListener('click', function () {
+            addressModal.hide();
+        });
+    });
 });
 
 function saveDataAndCloseModal() {
-  var phoneNumber = document.getElementById('storePhoneNumber').value;
-  var password = document.getElementById('storePassword').value;
-  var confirmPassword = document.getElementById('confirmPassword').value;
-  var passwordMismatch = document.getElementById('passwordMismatch');
+    var userName = document.getElementById('userUserName').value;
+    var name = document.getElementById('userName').value;
+    var lastName = document.getElementById('userLastName').value;
+    var genderOptions = document.querySelector('input[name="genderOptions"]:checked').value;
+    var smokeOptions = document.querySelector('input[name="smokeOptions"]:checked').value;
+    var email = document.getElementById('userEmail').value;
+    var phoneNumber = document.getElementById('userPhoneNumber').value;
+    var password = document.getElementById('storePassword').value;
+    var confirmPassword = document.getElementById('confirmPassword').value;
+    var passwordMismatch = document.getElementById('passwordMismatch');
 
-  if (password === confirmPassword) {
-      alert('บันทึกข้อมูลเรียบร้อยแล้ว');
-      var addressModal = new bootstrap.Modal(document.getElementById('addressModal'));
-      addressModal.hide();
-      document.getElementById('addressForm').reset();
-      passwordMismatch.style.display = 'none';
-  } else {
-      passwordMismatch.style.display = 'block';
-  }
+    if (password === confirmPassword) {
+        alert('บันทึกข้อมูลเรียบร้อยแล้ว');
+        var addressModal = new bootstrap.Modal(document.getElementById('addressModal'));
+        addressModal.hide();
+        document.getElementById('addressForm').reset();
+        passwordMismatch.style.display = 'none';
+    } else {
+        passwordMismatch.style.display = 'block';
+    }
 }
