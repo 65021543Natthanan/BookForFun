1042eecd468367713a716541410884612051f293
AphiwitPala <155602266+AphiwitPala@users.noreply.github.com>
2024-02-12 20:43:57 +0700

    หน้าลงทะเบียนลิ้งเชื่อมไปหน้าหลักได้

--- User/sign up/signup.js
+++ User/sign up/signup.js
@@ -11,6 +11,13 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function saveDataAndCloseModal() {
+    var userName = document.getElementById('userUserName').value;
+    var name = document.getElementById('userName').value;
+    var lastName = document.getElementById('userLastName').value;
+    var genderOptions = document.querySelector('input[name="genderOptions"]:checked').value;
+    var smokeOptions = document.querySelector('input[name="smokeOptions"]:checked').value;
+    var email = document.getElementById('userEmail').value;
+    var phoneNumber = document.getElementById('userPhoneNumber').value;
     var password = document.getElementById('storePassword').value;
     var confirmPassword = document.getElementById('confirmPassword').value;
     var passwordMismatch = document.getElementById('passwordMismatch');
@@ -23,8 +30,6 @@ function saveDataAndCloseModal() {
         passwordMismatch.style.display = 'none';
         window.location.href = '../After login.html/index.html';
     } else {
-        // เมื่อรหัสผ่านไม่ตรงกันแสดงป้อปอัพ
-        alert('รหัสผ่านไม่ตรงกัน');
         passwordMismatch.style.display = 'block';
     }
 }
